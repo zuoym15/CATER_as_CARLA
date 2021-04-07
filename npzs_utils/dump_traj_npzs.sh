b7ac2f13fdb26dbdc06771323e8ae39c846869f1
Yiming Zuo <yzuo@matrix.ml.cmu.edu>
2021-04-06 23:22:05 -0400

    dump static camera video on matrix

--- npzs_utils/dump_traj_npzs.sh
+++ npzs_utils/dump_traj_npzs.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# DATA_DIR="/projects/katefgroup/datasets/cater/raw/aa_s300_c6_m10/"
-DATA_DIR="/projects/katefgroup/datasets/cater/raw/ab_s300_c2_m10_rc_2/"
+DATA_DIR="/projects/katefgroup/datasets/cater/raw/aa_s300_c6_m10/"
+# DATA_DIR="/projects/katefgroup/datasets/cater/raw/ab_s300_c2_m10_rc_2/"
+# DATA_DIR="/projects/katefgroup/datasets/cater/raw/ab_s300_c2_m10_rc"
 OUT_DIR="/projects/katefgroup/datasets/cater/npzs/"
 
 DATA_FORMAT="traj"
 
-SEQLEN=300
-NUM_CAM=2
+SEQLEN=2
+NUM_CAM=6
+depth_downsample_factor=1.0
 # INCR=1
 
 MOD="ab" # generate traj data
@@ -16,6 +18,8 @@ MOD="ad" # seqlen 300, and save shapelist info
 MOD="ae" # save action labels
 MOD="ag" # multiview data
 MOD="af" # multiview data part 2
+MOD="ah" # do not downsample pts
+MOD="ai" # again genertae data on aa raw, to get comparable results to our cvpr submission
 
 # OUT_DIR="/projects/katefgroup/datasets/carla/processed/npzs/surveil_multiview_${MOD}_s${SEQLEN}_i${INCR}"
 # mkdir -p ${OUT_DIR}
@@ -30,4 +34,5 @@ python dump_npzs.py \
        --data_format ${DATA_FORMAT} \
        --dump_base_dir ${OUT_DIR} \
        --output_dir ${DATA_DIR} \
-       --total_num_cameras ${NUM_CAM}
+       --total_num_cameras ${NUM_CAM} \
+       --depth_downsample_factor ${depth_downsample_factor}
